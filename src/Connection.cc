acb93044079264ebcb13a3d7d9a6e6613c36de4f
vagrantac <vagrantac@126.com>
2023-12-03 19:43:09 +0800

    day9

--- src/Connection.cc
+++ src/Connection.cc
@@ -1,17 +1,21 @@
 #include "Connection.h"
+#include "Buffer.h"
 #include "Channel.h"
 #include "Socket.h"
+#include "util.h"
 #include <string.h>
 #include <unistd.h>
 
 #define READ_BUFFER 1024
 
 Connection::Connection(EventLoop *_loop, Socket *_sock)
-    : loop(_loop), sock(_sock), channel(nullptr) {
+    : loop(_loop), sock(_sock), channel(nullptr), inBuffer(new std::string()),
+      readBuffer(nullptr) {
   channel = new Channel(loop, sock->getFd());
   std::function<void()> cb = std::bind(&Connection::echo, this, sock->getFd());
   channel->setCallback(cb);
   channel->enableReading();
+  readBuffer = new Buffer();
 }
 
 Connection::~Connection() {
@@ -25,14 +29,17 @@ void Connection::echo(int socket_fd) {
     bzero(&buf, sizeof(buf));
     ssize_t bytes_read = read(socket_fd, buf, sizeof(buf));
     if (bytes_read > 0) {
-      printf("message from client fd %d: %s\n", socket_fd, buf);
-      write(socket_fd, buf, sizeof(buf));
+      readBuffer->append(buf, bytes_read);
     } else if (bytes_read == -1 && errno == EINTR) {
       printf("continue reading");
       continue;
     } else if (bytes_read == -1 &&
                ((errno == EAGAIN) || (errno == EWOULDBLOCK))) {
-      printf("finish reading once, errno: %d\n", errno);
+      printf("finish reading once\n");
+      printf("message from client fd %d: %s\n", socket_fd, readBuffer->c_str());
+      errif(write(socket_fd, readBuffer->c_str(), readBuffer->size()) == -1,
+            "socket write error");
+      readBuffer->clear();
       break;
     } else if (bytes_read == 0) {
       printf("EOF, client fd %d disconnected\n", socket_fd);
